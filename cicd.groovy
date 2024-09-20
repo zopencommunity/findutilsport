44d45543bfc957c46bed8998dae3bf716cfa4dcc
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:04 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/findutilsport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/findutilsport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/findutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Find Utilities are the basic directory searching utilities of the GNU operating system.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/findutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Find Utilities are the basic directory searching utilities of the GNU operating system.' )]
   }
 }
